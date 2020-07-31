21fc65d45e47a578871ee9336f9905cf95a1d362
Immo Landwerth <immo@landwerth.net>
2020-07-31 09:48:05 -0700

    Organize SQL Project

---
+++ ApiCatalog/Scripts/SelectPackages.sql
@@ -0,0 +1,5 @@
+﻿SELECT	p.Name,
+		pv.Version
+FROM	Packages p
+			JOIN PackageVersions pv on pv.PackageId = p.PackageId
+ORDER	BY p.Name, pv.Version
---
+++ ApiCatalog/Scripts/Stats.sql
@@ -0,0 +1,9 @@
+﻿SELECT
+	[Apis]                = (SELECT COUNT(*) FROM [Apis]),
+	[Declarations]        = (SELECT COUNT(*) FROM [Declarations]),
+	[Assemblies]          = (SELECT COUNT(*) FROM [Assemblies]),
+	[Packages]            = (SELECT COUNT(*) FROM [Packages]),
+	[PackageVersions]     = (SELECT COUNT(*) FROM [PackageVersions]),
+	[PackageAssemblies]   = (SELECT COUNT(*) FROM [PackageAssemblies]),
+	[Frameworks]          = (SELECT COUNT(*) FROM [Frameworks]),
+	[PlatformAssemblies]  = (SELECT COUNT(*) FROM [PlatformAssemblies])
