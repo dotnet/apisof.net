6a121d9182620269227c9dd4e6adb6fe7d2ea49b
Immo Landwerth <immol@microsoft.com>
2021-06-03 16:06:22 -0700

    Add owner for Microsoft.Data.SqlClient

--- src/ApiCatalog/NuGet/DotnetPackageIndex.cs
+++ src/ApiCatalog/NuGet/DotnetPackageIndex.cs
@@ -15,6 +15,7 @@ namespace ApiCatalog
             "dotnetframework",
             "EntityFramework",
             "RoslynTeam",
+            "nugetsqltools"
             //"dotnetfoundation"
         };
 
