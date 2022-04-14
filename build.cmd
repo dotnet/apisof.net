0b33563c124cc3bd80dd26527b94e0d5fb09e69a
Immo Landwerth <immo@landwerth.net>
2022-04-13 22:17:09 -0700

    Ensure batch variables don't leak

--- build.cmd
+++ build.cmd
@@ -1,4 +1,5 @@
 @echo off
+setlocal
 
 set "SLNDIR=%~dp0src"
 dotnet build "%SLNDIR%\apisof.net.sln" --nologo
--- run.cmd
+++ run.cmd
@@ -1,4 +1,5 @@
 @echo off
+setlocal
 
 set "SLNDIR=%~dp0src"
 dotnet run --project "%SLNDIR%\apisof.net\apisof.net.csproj" --nologo
