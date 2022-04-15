d6fa71833bae426a195bc9f060eac72eb085d721
Immo Landwerth <immo@landwerth.net>
2022-04-15 10:46:23 -0700

    Speed up package indexing
    Instead of pulling down all package ids and version from nuget.org and then
    filtering it down to just the ones owned by us, start with the package ids
    from the owner mapping, filter down to the package ids owned by us and then
    ask for the latest version.
    
    This also ensures we're indexing the latest stable and the latest preview, if
    the preview is newer.

--- src/Terrajobst.ApiCatalog/NuGet/DotnetPackageIndex.cs
+++ src/Terrajobst.ApiCatalog/NuGet/DotnetPackageIndex.cs
@@ -1,4 +1,7 @@
-﻿using System.Xml.Linq;
+﻿using System.Collections.Concurrent;
+using System.Xml.Linq;
+using NuGet.Packaging.Core;
+using NuGet.Versioning;
 
 namespace Terrajobst.ApiCatalog;
 
@@ -20,17 +23,50 @@ public static class DotnetPackageIndex
         Console.WriteLine($"Fetching owner information...");
         var ownerInformation = await feed.GetOwnerMappingAsync();
 
-        Console.WriteLine($"Fetching packages...");
-        var packages = await feed.GetAllPackages();
-
         var packageDocument = new XDocument();
         var root = new XElement("packages");
         packageDocument.Add(root);
 
-        var filteredPackages = packages.Where(l => IsOwnedByDotNet(ownerInformation, l.Id))
-            .OrderBy(p => p);
+        var packageIds = ownerInformation.Keys
+                                         .ToHashSet(StringComparer.OrdinalIgnoreCase)
+                                         .Where(id => IsOwnedByDotNet(ownerInformation, id) &&
+                                                      PackageFilter.Default.IsMatch(id))
+                                         .ToArray();
 
-        foreach (var item in filteredPackages)
+        Console.WriteLine($"Found {packageIds.Length:N0} relevant platform package IDs.");
+
+        Console.WriteLine($"Filtering to latest versions...");
+
+        var filteredPackages = new ConcurrentBag<PackageIdentity>();
+
+        await Parallel.ForEachAsync(packageIds, async (packageId, _) =>
+        {
+            var versions = await feed.GetAllVersionsAsync(packageId);
+            var identities = versions.Select(v => new PackageIdentity(packageId, v))
+                                     .OrderByDescending(v => v.Version, VersionComparer.VersionReleaseMetadata)
+                                     .ToArray();
+
+            var latestStable = identities.FirstOrDefault(i => !i.Version.IsPrerelease);
+            var latestPrerelease = identities.FirstOrDefault(i => i.Version.IsPrerelease);
+
+            if (latestStable is not null && latestPrerelease is not null)
+            {
+                var stableIsNewer = VersionComparer.VersionReleaseMetadata.Compare(latestPrerelease.Version, latestStable.Version) <= 0;
+                if (stableIsNewer)
+                    latestPrerelease = null;
+            }
+
+            if (latestStable is not null)
+                filteredPackages.Add(latestStable);
+
+            if (latestPrerelease is not null)
+                filteredPackages.Add(latestPrerelease);
+        });
+        
+        Console.WriteLine($"Found {filteredPackages.Count:N0} platform package versions.");
+
+        foreach (var item in filteredPackages.OrderBy(pi => pi.Id)
+                                             .ThenBy(pi => pi.Version, VersionComparer.VersionReleaseMetadata))
         {
             var e = new XElement("package",
                 new XAttribute("id", item.Id),
--- src/Terrajobst.ApiCatalog/NuGet/NuGetFeed.cs
+++ src/Terrajobst.ApiCatalog/NuGet/NuGetFeed.cs
@@ -1,9 +1,4 @@
-﻿using System.Collections.Concurrent;
-using System.Net;
-using System.Net.Http.Json;
-
-using Newtonsoft.Json;
-
+﻿using System.Net.Http.Json;
 using NuGet.Common;
 using NuGet.Packaging;
 using NuGet.Packaging.Core;
@@ -21,81 +16,6 @@ public sealed class NuGetFeed
     }
 
     public string FeedUrl { get; }
-
-    public async Task<IReadOnlyList<PackageIdentity>> GetAllPackages(DateTimeOffset? since = null)
-    {
-        var sourceRepository = Repository.Factory.GetCoreV3(FeedUrl);
-        var serviceIndex = await sourceRepository.GetResourceAsync<ServiceIndexResourceV3>();
-        var catalogIndexUrl = serviceIndex.GetServiceEntryUri("Catalog/3.0.0")?.ToString();
-
-        if (catalogIndexUrl == null)
-            throw new InvalidOperationException("This feed doesn't support enumeration");
-
-        const int MaxDegreeOfParallelism = 64;
-
-        ThreadPool.SetMinThreads(MaxDegreeOfParallelism, completionPortThreads: 4);
-        ServicePointManager.DefaultConnectionLimit = MaxDegreeOfParallelism;
-        ServicePointManager.MaxServicePointIdleTime = 10000;
-
-        var handler = new HttpClientHandler
-        {
-            SslProtocols = System.Security.Authentication.SslProtocols.Tls12
-        };
-        using var httpClient = new HttpClient(handler);
-
-        var indexString = await httpClient.GetStringAsync(catalogIndexUrl);
-        var index = JsonConvert.DeserializeObject<CatalogIndex>(indexString);
-
-        // Find all pages in the catalog index.
-        var pageItems = new ConcurrentBag<CatalogPage>(index.Items);
-        var catalogLeaves = new ConcurrentBag<CatalogLeaf>();
-
-        var fetchLeafsTasks = RunInParallel(async () =>
-        {
-            while (pageItems.TryTake(out var pageItem))
-            {
-                if (since != null && pageItem.CommitTimeStamp < since.Value)
-                    continue;
-
-                var retryCount = 3;
-            Retry:
-                try
-                {
-                    // Download the catalog page and deserialize it.
-                    var pageString = await httpClient.GetStringAsync(pageItem.Url);
-                    var page = JsonConvert.DeserializeObject<CatalogPage>(pageString);
-
-                    foreach (var pageLeafItem in page.Items)
-                    {
-                        if (pageLeafItem.Type == "nuget:PackageDetails")
-                            catalogLeaves.Add(pageLeafItem);
-                    }
-                }
-                catch (Exception ex) when (retryCount > 0)
-                {
-                    retryCount--;
-                    Console.Error.WriteLine($"error: {ex.Message}, retries left = {retryCount}");
-                    goto Retry;
-                }
-            }
-        });
-
-        await Task.WhenAll(fetchLeafsTasks);
-
-        return catalogLeaves
-            .Select(l => new PackageIdentity(l.Id, NuGetVersion.Parse(l.Version)))
-            .Distinct()
-            .OrderBy(p => p.Id)
-            .ThenBy(p => p.Version)
-            .ToArray();
-
-        static List<Task> RunInParallel(Func<Task> work)
-        {
-            return Enumerable.Range(0, MaxDegreeOfParallelism)
-                .Select(i => work())
-                .ToList();
-        }
-    }
 
     public async Task<IReadOnlyList<NuGetVersion>> GetAllVersionsAsync(string packageId)
     {
@@ -158,36 +78,5 @@ public sealed class NuGetFeed
         var httpClient = new HttpClient();
         var url = "https://nugetprodusncazuresearch.blob.core.windows.net/v3-azuresearch-014/owners/owners.v2.json";
         return httpClient.GetFromJsonAsync<Dictionary<string, string[]>>(url);
-    }
-
-    private abstract class CatalogEntity
-    {
-        [JsonProperty("@id")]
-        public string Url { get; set; }
-
-        [JsonProperty("commitTimeStamp")]
-        public DateTime CommitTimeStamp { get; set; }
-    }
-
-    private sealed class CatalogIndex : CatalogEntity
-    {
-        public List<CatalogPage> Items { get; set; }
-    }
-
-    private sealed class CatalogPage : CatalogEntity
-    {
-        public List<CatalogLeaf> Items { get; set; }
-    }
-
-    private sealed class CatalogLeaf : CatalogEntity
-    {
-        [JsonProperty("nuget:id")]
-        public string Id { get; set; }
-
-        [JsonProperty("nuget:version")]
-        public string Version { get; set; }
-
-        [JsonProperty("@type")]
-        public string Type { get; set; }
     }
 }
