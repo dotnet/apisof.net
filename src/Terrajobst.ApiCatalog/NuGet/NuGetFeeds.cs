3b1e1b8bc7985aaf955e6c31f9fb7d4a34a9142d
Immo Landwerth <immo@landwerth.net>
2023-07-26 12:29:00 -0700

    Add .NET 8 to API Catalog

--- src/Terrajobst.ApiCatalog/NuGet/NuGetFeeds.cs
+++ src/Terrajobst.ApiCatalog/NuGet/NuGetFeeds.cs
@@ -4,5 +4,6 @@ public static class NuGetFeeds
 {
     public static string NuGetOrg => "https://api.nuget.org/v3/index.json";
     public static string NightlyDotnet7 => "https://pkgs.dev.azure.com/dnceng/public/_packaging/dotnet7/nuget/v3/index.json";
+    public static string NightlyDotnet8 => "https://pkgs.dev.azure.com/dnceng/public/_packaging/dotnet8/nuget/v3/index.json";
     public static string NightlyXamarin => "https://pkgs.dev.azure.com/azure-public/vside/_packaging/xamarin-impl/nuget/v3/index.json";
 }
