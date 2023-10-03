664a6cdf2324d6660f6a0e0a46d203ef2aeb39af
Immo Landwerth <immol@microsoft.com>
2023-10-03 13:59:52 -0700

    Move to .NET 7

--- src/NetUpgradePlannerTelemetry/StoreTelemetryFunction.cs
+++ src/NetUpgradePlannerTelemetry/StoreTelemetryFunction.cs
@@ -1,15 +1,10 @@
-using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Threading.Tasks;
+using System.Net;
+using System.Web;
 
 using Azure.Storage.Blobs;
 
-using Microsoft.AspNetCore.Http;
-using Microsoft.AspNetCore.Mvc;
-using Microsoft.Azure.WebJobs;
-using Microsoft.Azure.WebJobs.Extensions.Http;
+using Microsoft.Azure.Functions.Worker;
+using Microsoft.Azure.Functions.Worker.Http;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
 
@@ -17,46 +12,48 @@ namespace NetUpgradePlannerTelemetry;
 
 public sealed class StoreTelemetryFunction
 {
+    private readonly ILogger _logger;
     private readonly IConfiguration _configuration;
 
-    public StoreTelemetryFunction(IConfiguration configuration)
+    public StoreTelemetryFunction(IConfiguration configuration, ILoggerFactory loggerFactory)
     {
+        _logger = loggerFactory.CreateLogger<StoreTelemetryFunction>();
         _configuration = configuration;
     }
 
-    [FunctionName("store-telemetry")]
-    public async Task<IActionResult> Run(
-        [HttpTrigger(AuthorizationLevel.Anonymous, "post", Route = null)] HttpRequest request,
-        ILogger logger)
+    [Function("store-telemetry")]
+    public async Task<HttpResponseData> Run(
+        [HttpTrigger(AuthorizationLevel.Anonymous, "post", Route = null)] HttpRequestData request)
     {
-        var fingerprintValues = request.Query["fingerprint"];
-        if (fingerprintValues.Count != 1)
-            return new BadRequestResult();
+        var query = HttpUtility.ParseQueryString(request.Url.Query);
+        var fingerprintValues = query.GetValues("fingerprint");
+        if (fingerprintValues?.Length != 1)
+            return request.CreateResponse(HttpStatusCode.BadRequest);
 
         var fingerprint = fingerprintValues.Single();
         if (fingerprint.Length != 64)
-            return new BadRequestResult();
+            return request.CreateResponse(HttpStatusCode.BadRequest);
 
         foreach (var c in fingerprint)
         {
             var isHex = c >= '0' && c <= '9' ||
                         c >= 'A' && c <= 'F';
             if (!isHex)
-                return new BadRequestResult();
+                return request.CreateResponse(HttpStatusCode.BadRequest);
         }
 
         var apis = new List<Guid>();
-        using var reader = new StreamReader(request.Body);            
+        using var reader = new StreamReader(request.Body);
         while (await reader.ReadLineAsync() is string line)
         {
             if (Guid.TryParse(line.Trim(), out var api))
                 apis.Add(api);
             else
-                return new BadRequestResult();
+                return request.CreateResponse(HttpStatusCode.BadRequest);
         }
 
         if (apis.Count == 0)
-            return new BadRequestResult();
+            return request.CreateResponse(HttpStatusCode.BadRequest);
 
         var connectionString = _configuration["AzureStorageConnectionString"];
 
@@ -67,8 +64,8 @@ public sealed class StoreTelemetryFunction
         var blobData = BinaryData.FromString(blobText);
         await blobClient.UploadAsync(blobData, overwrite: true);
 
-        logger.LogInformation($"Stored {apis.Count} APIs for {fingerprint}.");
+        _logger.LogInformation($"Stored {apis.Count} APIs for {fingerprint}.");
 
-        return new OkResult();
+        return request.CreateResponse(HttpStatusCode.OK);
     }
 }
