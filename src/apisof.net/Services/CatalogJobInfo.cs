6ac096d4ec427d5d332bcebb1c4bea8f2ac2cfdd
Immo Landwerth <immol@microsoft.com>
2024-04-03 19:36:52 -0700

    Make CatalogJobInfo immutable

--- src/apisof.net/Services/CatalogJobInfo.cs
+++ src/apisof.net/Services/CatalogJobInfo.cs
@@ -2,14 +2,23 @@
 
 public sealed class CatalogJobInfo
 {
-    public static CatalogJobInfo Empty { get; } = new()
+    public static CatalogJobInfo Empty { get; } = new();
+
+    private CatalogJobInfo()
     {
-        Date = DateTimeOffset.MinValue,
-        Success = true,
-        DetailsUrl = "https://github.com/dotnet/apisof.net/actions/workflows/gen-catalog.yml"
-    };
-    
-    public required DateTimeOffset Date { get; set; }
-    public required bool Success { get; set; }
-    public required string DetailsUrl { get; set; }
+        Date = DateTimeOffset.MinValue;
+        Success = true;
+        DetailsUrl = "https://github.com/dotnet/apisof.net/actions/workflows/gen-catalog.yml";
+    }
+
+    public CatalogJobInfo(DateTimeOffset date, bool success, string detailsUrl)
+    {
+        Date = date;
+        Success = success;
+        DetailsUrl = detailsUrl;
+    }
+
+    public DateTimeOffset Date { get; }
+    public bool Success { get; }
+    public string DetailsUrl { get; }
 }
