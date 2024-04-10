656e10b400b9670b173ad926719d1074e8752b52
Immo Landwerth <immol@microsoft.com>
2024-04-09 19:14:30 -0700

    Use file scoped namespace

--- src/Terrajobst.UsageCrawling/ApiKey.cs
+++ src/Terrajobst.UsageCrawling/ApiKey.cs
@@ -2,94 +2,93 @@ using System.Diagnostics;
 using System.Security.Cryptography;
 using System.Text;
 
-namespace Terrajobst.UsageCrawling
+namespace Terrajobst.UsageCrawling;
+
+public readonly struct ApiKey : IEquatable<ApiKey>, IComparable<ApiKey>, IComparable
 {
-    public readonly struct ApiKey : IEquatable<ApiKey>, IComparable<ApiKey>, IComparable
+    public ApiKey(string documentationId)
     {
-        public ApiKey(string documentationId)
+        ThrowIfNull(documentationId);
+
+        Guid = ComputeGuid(documentationId);
+        DocumentationId = documentationId;
+    }
+
+    public Guid Guid { get; }
+
+    public string DocumentationId { get; }
+
+    public bool Equals(ApiKey other)
+    {
+        return Guid.Equals(other.Guid);
+    }
+
+    public override bool Equals(object? obj)
+    {
+        return obj is ApiKey other &&
+               Equals(other);
+    }
+
+    public override int GetHashCode()
+    {
+        return Guid.GetHashCode();
+    }
+
+    public int CompareTo(ApiKey other)
+    {
+        return string.CompareOrdinal(DocumentationId, other.DocumentationId);
+    }
+
+    public int CompareTo(object? obj)
+    {
+        if (obj is ApiKey other)
+            return CompareTo(other);
+
+        return -1;
+    }
+
+    private static Guid ComputeGuid(string documentationId)
+    {
+        const int maxBytesOnStack = 256;
+
+        var encoding = Encoding.UTF8;
+        var maxByteCount = encoding.GetMaxByteCount(documentationId.Length);
+
+        if (maxByteCount <= maxBytesOnStack)
         {
-            ThrowIfNull(documentationId);
-
-            Guid = ComputeGuid(documentationId);
-            DocumentationId = documentationId;
+            var buffer = (Span<byte>)stackalloc byte[maxBytesOnStack];
+            var written = encoding.GetBytes(documentationId, buffer);
+            var utf8Bytes = buffer[..written];
+            return HashData(utf8Bytes);
         }
-
-        public Guid Guid { get; }
-
-        public string DocumentationId { get; }
-
-        public bool Equals(ApiKey other)
+        else
         {
-            return Guid.Equals(other.Guid);
+            var utf8Bytes = encoding.GetBytes(documentationId);
+            return HashData(utf8Bytes);
         }
+    }
 
-        public override bool Equals(object? obj)
-        {
-            return obj is ApiKey other &&
-                   Equals(other);
-        }
+    private static Guid HashData(ReadOnlySpan<byte> bytes)
+    {
+        var hashBytes = (Span<byte>)stackalloc byte[16];
+        var written = MD5.HashData(bytes, hashBytes);
+        Debug.Assert(written == hashBytes.Length);
 
-        public override int GetHashCode()
-        {
-            return Guid.GetHashCode();
-        }
+        return new Guid(hashBytes);
+    }
 
-        public int CompareTo(ApiKey other)
-        {
-            return string.CompareOrdinal(DocumentationId, other.DocumentationId);
-        }
+    public static bool operator ==(ApiKey left, ApiKey right)
+    {
+        return left.Equals(right);
+    }
 
-        public int CompareTo(object? obj)
-        {
-            if (obj is ApiKey other)
-                return CompareTo(other);
+    public static bool operator !=(ApiKey left, ApiKey right)
+    {
+        return !(left == right);
+    }
 
-            return -1;
-        }
-
-        private static Guid ComputeGuid(string documentationId)
-        {
-            const int maxBytesOnStack = 256;
-
-            var encoding = Encoding.UTF8;
-            var maxByteCount = encoding.GetMaxByteCount(documentationId.Length);
-
-            if (maxByteCount <= maxBytesOnStack)
-            {
-                var buffer = (Span<byte>)stackalloc byte[maxBytesOnStack];
-                var written = encoding.GetBytes(documentationId, buffer);
-                var utf8Bytes = buffer[..written];
-                return HashData(utf8Bytes);
-            }
-            else
-            {
-                var utf8Bytes = encoding.GetBytes(documentationId);
-                return HashData(utf8Bytes);
-            }
-        }
-
-        private static Guid HashData(ReadOnlySpan<byte> bytes)
-        {
-            var hashBytes = (Span<byte>)stackalloc byte[16];
-            var written = MD5.HashData(bytes, hashBytes);
-            Debug.Assert(written == hashBytes.Length);
-
-            return new Guid(hashBytes);
-        }
-
-        public static bool operator ==(ApiKey left, ApiKey right)
-        {
-            return left.Equals(right);
-        }
-
-        public static bool operator !=(ApiKey left, ApiKey right)
-        {
-            return !(left == right);
-        }
-
-        public override string ToString()
-        {
-            return DocumentationId;
-        }
+    public override string ToString()
+    {
+        return DocumentationId;
     }
 }
--- src/Terrajobst.UsageCrawling/CrawlerResults.cs
+++ src/Terrajobst.UsageCrawling/CrawlerResults.cs
@@ -1,24 +1,23 @@
-namespace Terrajobst.UsageCrawling
+namespace Terrajobst.UsageCrawling;
+
+public sealed class CrawlerResults
 {
-    public sealed class CrawlerResults
+    public CrawlerResults(IReadOnlyDictionary<ApiKey, int> data)
     {
-        public CrawlerResults(IReadOnlyDictionary<ApiKey, int> data)
-        {
-            Data = data;
-        }
+        Data = data;
+    }
 
-        public IReadOnlyDictionary<ApiKey, int> Data { get; }
+    public IReadOnlyDictionary<ApiKey, int> Data { get; }
 
-        public async Task WriteGuidsAsync(string fileName)
-        {
-            await using var writer = File.CreateText(fileName);
-            await WriteGuidsAsync(writer);
-        }
+    public async Task WriteGuidsAsync(string fileName)
+    {
+        await using var writer = File.CreateText(fileName);
+        await WriteGuidsAsync(writer);
+    }
 
-        public async Task WriteGuidsAsync(TextWriter writer)
-        {
-            foreach (var key in Data.Keys.OrderBy(k => k))
-                await writer.WriteLineAsync(key.Guid.ToString("N"));
-        }
+    public async Task WriteGuidsAsync(TextWriter writer)
+    {
+        foreach (var key in Data.Keys.OrderBy(k => k))
+            await writer.WriteLineAsync(key.Guid.ToString("N"));
     }
 }
