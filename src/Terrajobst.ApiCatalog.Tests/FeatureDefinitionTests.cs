48449dff5872e500630708772e37e75205ee4faa
Immo Landwerth <immol@microsoft.com>
2024-04-16 16:51:01 -0700

    Test for duplicated feature ids

---
+++ src/Terrajobst.ApiCatalog.Tests/FeatureDefinitionTests.cs
@@ -0,0 +1,40 @@
+﻿using Terrajobst.ApiCatalog.Features;
+
+namespace Terrajobst.ApiCatalog.Tests;
+
+public class FeatureDefinitionTests
+{
+    [Fact]
+    public void FeatureDefinition_NoDuplicateIds()
+    {
+        var guidSet = new Dictionary<Guid, FeatureDefinition>();
+
+        foreach (var globalFeature in FeatureDefinition.GlobalFeatures)
+        {
+            if (!guidSet.TryGetValue(globalFeature.FeatureId, out var existingFeature))
+            {
+                guidSet.Add(globalFeature.FeatureId, globalFeature);
+            }
+            else
+            {
+                Assert.Fail($"The feature '{globalFeature.Name}' has the same GUID as '{existingFeature.Name}'");
+            }
+        }
+
+        var parameter = Guid.Parse("6769197f-ec9c-4848-af2b-2f50c35630f5");
+
+        foreach (var parameterizedFeature in FeatureDefinition.ApiFeatures)
+        {
+            var featureId = parameterizedFeature.GetFeatureId(parameter);
+
+            if (!guidSet.TryGetValue(featureId, out var existingFeature))
+            {
+                guidSet.Add(featureId, parameterizedFeature);
+            }
+            else
+            {
+                Assert.Fail($"The feature '{parameterizedFeature.Name}' has the same GUID as '{existingFeature.Name}'");
+            }
+        }
+    }
+}
