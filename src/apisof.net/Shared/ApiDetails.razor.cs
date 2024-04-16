053074f7942af34f02fdd51a583d29f97d799c11
Immo Landwerth <immol@microsoft.com>
2024-04-16 15:57:41 -0700

    Split property and event accessors

--- src/apisof.net/Shared/ApiDetails.razor.cs
+++ src/apisof.net/Shared/ApiDetails.razor.cs
@@ -119,6 +119,44 @@ public partial class ApiDetails
                 usages.Add((usageSource, feature, percentage));
         }
 
+        if (Api.Kind == ApiKind.Property)
+        {
+            var getter = Api.Children.Where(a => a.Kind == ApiKind.PropertyGetter).Cast<ApiModel?>().FirstOrDefault();
+            var setter = Api.Children.Where(a => a.Kind == ApiKind.PropertySetter).Cast<ApiModel?>().FirstOrDefault();
+
+            // Let's only split usage into get/set when both are present. 
+            if (getter is not null && setter is not null)
+            {
+                var getterFeatureId = FeatureDefinition.ApiUsage.GetFeatureId(getter.Value.Guid);
+                foreach (var (usageSource, percentage) in usageData.GetUsage(getterFeatureId))
+                    usages.Add((usageSource, SyntheticFeatureDefinition.PropertyGet, percentage));
+
+                var setterFeatureId = FeatureDefinition.ApiUsage.GetFeatureId(setter.Value.Guid);
+                foreach (var (usageSource, percentage) in usageData.GetUsage(setterFeatureId))
+                    usages.Add((usageSource, SyntheticFeatureDefinition.PropertySet, percentage));
+            }
+        }
+
+        if (Api.Kind == ApiKind.Event)
+        {
+            var adder = Api.Children.Where(a => a.Kind == ApiKind.EventAdder).Cast<ApiModel?>().FirstOrDefault();
+            var remover = Api.Children.Where(a => a.Kind == ApiKind.EventRemover).Cast<ApiModel?>().FirstOrDefault();
+
+            if (adder is not null)
+            {
+                var adderFeatureId = FeatureDefinition.ApiUsage.GetFeatureId(adder.Value.Guid);
+                foreach (var (usageSource, percentage) in usageData.GetUsage(adderFeatureId))
+                    usages.Add((usageSource, SyntheticFeatureDefinition.EventAdd, percentage));
+            }
+
+            if (remover is not null)
+            {
+                var removerFeatureId = FeatureDefinition.ApiUsage.GetFeatureId(remover.Value.Guid);
+                foreach (var (usageSource, percentage) in usageData.GetUsage(removerFeatureId))
+                    usages.Add((usageSource, SyntheticFeatureDefinition.EventRemove, percentage));
+            }
+        }
+
         return usages.GroupBy(u => u.Source)
                      .Select(g => (g.Key, (IReadOnlyList<(FeatureDefinition, float)>)g.Select(t => (t.Feature, t.Percentage)).ToArray()))
                      .ToArray();
@@ -246,6 +284,39 @@ public partial class ApiDetails
                 ? Link.For(ExtensionMethod.Value, selected)
                 : Link.For(Api, selected);
             NavigationManager.NavigateTo(link);
+        }
+    }
+
+    private static class SyntheticFeatureDefinition
+    {
+        public static FeatureDefinition PropertyGet { get; } = new PropertyGetFeatureDefinition(); 
+        public static FeatureDefinition PropertySet { get; } = new PropertySetFeatureDefinition(); 
+
+        public static FeatureDefinition EventAdd { get; } = new EventAddFeatureDefinition(); 
+        public static FeatureDefinition EventRemove { get; } = new EventRemoveFeatureDefinition(); 
+
+        private sealed class PropertyGetFeatureDefinition : FeatureDefinition
+        {
+            public override string Name => "Get property";
+            public override string Description => "Percentage of applications/packages that read this property";
+        }
+    
+        private sealed class PropertySetFeatureDefinition : FeatureDefinition
+        {
+            public override string Name => "Set property";
+            public override string Description => "Percentage of applications/packages that write this property";
+        }
+
+        private sealed class EventAddFeatureDefinition : FeatureDefinition
+        {
+            public override string Name => "Subscribe to this event";
+            public override string Description => "Percentage of applications/packages that subscribe to this event";
+        }
+    
+        private sealed class EventRemoveFeatureDefinition : FeatureDefinition
+        {
+            public override string Name => "Unsubscribe from this event";
+            public override string Description => "Percentage of applications/packages that unsubscribe to this event";
         }
     }
 }
