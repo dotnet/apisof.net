8d1f7a065d2c9999be2d7204012e2fdd09babe9e
Immo Landwerth <immol@microsoft.com>
2024-04-15 21:49:32 -0700

    Add nav drop down for advanced pages
    Also aligned the look & feel of those pages.

---
+++ src/apisof.net/Pages/Version.razor.cs
@@ -0,0 +1,24 @@
+﻿using System.Reflection;
+
+namespace ApisOfDotNet.Pages;
+
+public partial class Version
+{
+    private string? _commit;
+    private int _catalogVersion;
+
+    protected override void OnInitialized()
+    {
+        var informationalVersion = GetType().Assembly.GetCustomAttributesData()
+                                                     .Where(ca => ca.AttributeType == typeof(AssemblyInformationalVersionAttribute))
+                                                     .SelectMany(ca => ca.ConstructorArguments.Select(a => a.Value as string))
+                                                     .FirstOrDefault(string.Empty)!;
+
+        var indexOfPlus = informationalVersion.IndexOf('+');
+        _commit = indexOfPlus >= 0
+            ? informationalVersion.Substring(indexOfPlus + 1)
+            : null;
+
+        _catalogVersion = CatalogService.Catalog.FormatVersion;
+    }
+}
