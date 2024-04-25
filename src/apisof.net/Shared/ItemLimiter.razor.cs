eaf2fdcb599ce0403556045aa97ab53f74d31e34
Immo Landwerth <immol@microsoft.com>
2024-04-24 20:39:49 -0700

    Add an output limiter

---
+++ src/apisof.net/Shared/ItemLimiter.razor.cs
@@ -0,0 +1,30 @@
+using Microsoft.AspNetCore.Components;
+
+namespace ApisOfDotNet.Shared;
+
+[CascadingTypeParameter(nameof(TItem))]
+public partial class ItemLimiter<TItem>
+{
+    private bool _showMore;
+    private TItem[]? _items;
+    
+    [Parameter, EditorRequired]
+    public required IEnumerable<TItem> ItemSource { get; set; }
+
+    [Parameter]
+    public int Limit { get; set; } = 20;
+    
+    [Parameter]
+    public required RenderFragment<TItem> ChildContent { get; set; }
+
+    private TItem[] Items
+    {
+        get
+        {
+            if (_items is null)
+                _items = ItemSource.ToArray();
+
+            return _items;
+        }
+    }
+}
