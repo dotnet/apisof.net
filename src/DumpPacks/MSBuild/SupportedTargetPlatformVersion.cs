73e00035627e451c2fd9050ac99e5e9a295b6ded
Immo Landwerth <immol@microsoft.com>
2024-04-29 21:52:14 -0700

    Modify DumpPacks to output supported OS Versions as well

---
+++ src/DumpPacks/MSBuild/SupportedTargetPlatformVersion.cs
@@ -0,0 +1,61 @@
+﻿using System.Xml.Linq;
+
+public sealed class SupportedTargetPlatformVersion
+{
+    public SupportedTargetPlatformVersion(string platform, Version version)
+    {
+        ThrowIfNullOrEmpty(platform);
+        ThrowIfNull(version);
+        Platform = platform;
+        Version = version;
+    }
+
+    public string Platform { get; }
+
+    public Version Version { get; }
+
+    public static IReadOnlyList<SupportedTargetPlatformVersion> Load(string path)
+    {
+        var result = new List<SupportedTargetPlatformVersion>();
+
+        var files = Directory.GetFiles(path, "*", SearchOption.AllDirectories);
+        foreach (var file in files)
+        {
+            var extension = Path.GetExtension(file);
+            var hasMSBuildExtension = string.Equals(extension, ".props", StringComparison.OrdinalIgnoreCase) ||
+                                      string.Equals(extension, ".targets", StringComparison.OrdinalIgnoreCase);
+
+            if (!hasMSBuildExtension)
+                continue;
+
+            try
+            {
+                const string ElementSuffix = "SdkSupportedTargetPlatformVersion";
+
+                var document = XDocument.Load(file);
+                var elements = document.Descendants().Where(e => e.Name.LocalName.EndsWith(ElementSuffix));
+
+                foreach (var e in elements)
+                {
+                    var platformName = e.Name.LocalName.Replace(ElementSuffix, string.Empty);
+                    var platformVersionText = e.Attribute("Include")?.Value;
+
+                    if (string.IsNullOrEmpty(platformName) ||
+                        string.IsNullOrEmpty(platformVersionText) ||
+                        !Version.TryParse(platformVersionText, out var platformVersion))
+                        continue;
+
+                    var supportedPlatformVersion = new SupportedTargetPlatformVersion(platformName, platformVersion);
+                    result.Add(supportedPlatformVersion);
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"error: Can't read '{file}': {ex.Message}");
+            }
+
+        }
+
+        return result.ToArray();
+    }
+}
--- src/DumpPacks/Program.cs
+++ src/DumpPacks/Program.cs
@@ -7,22 +7,35 @@ Console.WriteLine("//");
 Console.WriteLine("// Built-in Packs");
 Console.WriteLine("//");
 
-var sdksRoot = Path.Join(dotnetDirectory, "sdk");
-
-var references = KnownFrameworkReference.Load(sdksRoot);
-
-foreach (var frameworkGroup in references.GroupBy(f => f.TargetFramework)
-                                         .OrderBy(g => g.Key.Framework)
-                                         .ThenBy(g => g.Key.Version))
+foreach (var sdkDirectory in GetSdkDirectories(dotnetDirectory))
 {
-    Console.WriteLine($"// {frameworkGroup.Key.GetShortFolderName()}");
+    var version = NuGetVersion.Parse(Path.GetFileName(sdkDirectory));
 
-    foreach (var packGroup in frameworkGroup.GroupBy(r => r.TargetingPackName)
-                                            .OrderBy(p => p.Key))
+    Console.WriteLine($"// .NET SDK {version}");
+
+    var references = KnownFrameworkReference.Load(sdkDirectory);
+
+    foreach (var frameworkGroup in references.GroupBy(f => f.TargetFramework)
+                                             .OrderBy(g => g.Key.Framework)
+                                             .ThenBy(g => g.Key.Version))
     {
-        var pack = packGroup.MaxBy(p => p.TargetingPackVersion)!;
-        Console.WriteLine($"{pack.TargetingPackName}, {pack.TargetingPackVersion}");
+        Console.WriteLine($"// {frameworkGroup.Key.GetShortFolderName()}");
+
+        foreach (var packGroup in frameworkGroup.GroupBy(r => r.TargetingPackName)
+                                                .OrderBy(p => p.Key))
+        {
+            var pack = packGroup.MaxBy(p => p.TargetingPackVersion)!;
+            Console.WriteLine($"{pack.TargetingPackName}, {pack.TargetingPackVersion}");
+        }
     }
+
+    var supportedVersions = SupportedTargetPlatformVersion
+        .Load(sdkDirectory)
+        .GroupBy(v => v.Platform)
+        .Select(g => (g.Key, string.Join(", ", g.Select(v => v.Version).Distinct().Order())));
+
+    foreach (var (platform, versionList) in supportedVersions)
+        Console.WriteLine($"{platform}: {versionList}");
 }
 
 Console.WriteLine("//");
@@ -30,6 +43,7 @@ Console.WriteLine("// Workload Packs");
 Console.WriteLine("//");
 
 var manifestsRoot = Path.Join(dotnetDirectory, "sdk-manifests");
+var packsRoot = Path.Join(dotnetDirectory, "packs");
 
 foreach (var versionDirectory in Directory.GetDirectories(manifestsRoot))
 {
@@ -60,4 +74,55 @@ foreach (var versionDirectory in Directory.GetDirectories(manifestsRoot))
                 Console.WriteLine($"{aliasTo}, {pack.Version} ({pack.Kind}): {workloadNames}");
         }
     }
+
+    var platformVersions = new Dictionary<string, SortedSet<Version>>(StringComparer.OrdinalIgnoreCase);
+
+    foreach (var (pack, workloads) in environment.GetFlattenedPacks())
+    {
+        if (pack.Kind is not PackKind.Sdk)
+            continue;
+
+        var packNames = (string[]) [pack.Name, ..pack.AliasTo.Select(kv => kv.Value)];
+
+        foreach (var packName in packNames.Distinct())
+        {
+            var sdkDirectory = Path.Join(packsRoot, packName, pack.Version);
+            if (!Directory.Exists(sdkDirectory))
+                continue;
+
+            var supportedVersions = SupportedTargetPlatformVersion.Load(sdkDirectory).GroupBy(v => v.Platform);
+            if (!supportedVersions.Any())
+                continue;
+
+            foreach (var platformGroup in supportedVersions)
+            {
+                var platform = platformGroup.Key;
+
+                if (!platformVersions.TryGetValue(platform, out var versions))
+                {
+                    versions = new();
+                    platformVersions.Add(platform, versions);
+                }
+
+                versions.UnionWith(platformGroup.Select(v => v.Version));
+            }
+        }
+    }
+
+    foreach (var (platform, versions) in platformVersions)
+    {
+        var versionList = string.Join(", ", versions);
+        Console.WriteLine($"{platform}: {versionList}");
+    }
+}
+
+static IReadOnlyList<string> GetSdkDirectories(string dotnetDirectory)
+{
+    var sdkRoot = Path.Join(dotnetDirectory, "sdk");
+    return Directory.GetDirectories(sdkRoot)
+                    .Select(d => (Path: d, Version: NuGetVersion.Parse(Path.GetFileName(d))))
+                    .GroupBy(t => (t.Version.Major, t.Version.Minor))
+                    .Select(g => g.OrderByDescending(t => t.Version).First())
+                    .Select(t => t.Path)
+                    .ToArray();
 }
