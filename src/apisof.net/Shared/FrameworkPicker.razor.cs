cbf50ed16cd43f2b97a81ac4e84422531bb9b974
Immo Landwerth <immol@microsoft.com>
2024-05-03 18:31:23 -0700

    Suppress nullable warning

--- src/apisof.net/Shared/FrameworkPicker.razor.cs
+++ src/apisof.net/Shared/FrameworkPicker.razor.cs
@@ -17,7 +17,7 @@ public partial class FrameworkPicker
     [Parameter]
     public EventCallback<NuGetFramework?> SelectedChanged { get; set; }
 
-    private FrameworkModel Model { get; set; }
+    private FrameworkModel Model { get; set; } = null!;
 
     protected override void OnInitialized()
     {
