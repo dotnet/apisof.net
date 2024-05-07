4801cdafc38b82f3c9562ec4dddc92aa127047fd
Immo Landwerth <immol@microsoft.com>
2024-05-06 21:49:00 -0700

    Allow resolving platform-specific frameworks without a version

--- src/Terrajobst.ApiCatalog.Generation/Frameworks/PackBasedFrameworkLocator.cs
+++ src/Terrajobst.ApiCatalog.Generation/Frameworks/PackBasedFrameworkLocator.cs
@@ -25,35 +25,56 @@ public sealed class PackBasedFrameworkLocator : FrameworkLocator
         if (_mappings.TryGetValue(packageFramework, out var mappingPaths))
             return mappingPaths;
 
-        if (packageFramework.HasPlatform && packageFramework.PlatformVersion != FrameworkConstants.EmptyVersion)
+        if (packageFramework.HasPlatform)
         {
-            // If the framework has a platform version, we don't necessarily find an exact match.
-            //
-            // The mappings we have is for what platform version we ship in-box. For example, we might
-            // ship net6.0-windows7.0, but the package might target net6.0-windows10.0.12345. In that
-            // case we just return the framework assemblies for net6.0-windows7.0.
-            //
-            // If we ship multiple platform version (e.g. net6.0-windows7.0, net6.0-windows8.0) we'll
-            // return the highest one that is still lower or equal to the one the package targets
-            // (e.g. net6.0-windows8.0).
+            if (packageFramework.PlatformVersion == FrameworkConstants.EmptyVersion)
+            {
+                // If the framework has no version number, we just return the lowest plaform version
+                // the framework supports.
 
-            var mapping = _mappings.Where(kv => SameFrameworkSamePlatformLowerOrEqualPlatformVersion(kv.Key, packageFramework))
-                                   .OrderByDescending(kv => kv.Key.PlatformVersion)
-                                   .Cast<KeyValuePair<NuGetFramework, string[]>?>()
-                                   .FirstOrDefault();
+                var mapping = _mappings.Where(kv => SameFrameworkSamePlatform(kv.Key, packageFramework))
+                                       .OrderBy(kv => kv.Key.PlatformVersion)
+                                       .Cast<KeyValuePair<NuGetFramework, string[]>?>()
+                                       .FirstOrDefault();
 
-            if (mapping is not null)
-                return mapping.Value.Value;
+                if (mapping is not null)
+                    return mapping.Value.Value;
+            }
+            else
+            {
+                // If the framework has a platform version, we don't necessarily find an exact match.
+                //
+                // The mappings we have is for what platform version we ship in-box. For example, we might
+                // ship net6.0-windows7.0, but the package might target net6.0-windows10.0.12345. In that
+                // case we just return the framework assemblies for net6.0-windows7.0.
+                //
+                // If we ship multiple platform version (e.g. net6.0-windows7.0, net6.0-windows8.0) we'll
+                // return the highest one that is still lower or equal to the one the package targets
+                // (e.g. net6.0-windows8.0).
+
+                var mapping = _mappings.Where(kv => SameFrameworkSamePlatformLowerOrEqualPlatformVersion(kv.Key, packageFramework))
+                                       .OrderByDescending(kv => kv.Key.PlatformVersion)
+                                       .Cast<KeyValuePair<NuGetFramework, string[]>?>()
+                                       .FirstOrDefault();
+
+                if (mapping is not null)
+                    return mapping.Value.Value;
+            }
         }
 
         return null;
     }
 
-    private static bool SameFrameworkSamePlatformLowerOrEqualPlatformVersion(NuGetFramework left, NuGetFramework right)
+    private static bool SameFrameworkSamePlatform(NuGetFramework left, NuGetFramework right)
     {
         return left.HasPlatform && right.HasPlatform &&
                string.Equals(left.Framework, right.Framework, StringComparison.OrdinalIgnoreCase) &&
-               string.Equals(left.Platform, right.Platform, StringComparison.OrdinalIgnoreCase) &&
+               string.Equals(left.Platform, right.Platform, StringComparison.OrdinalIgnoreCase);
+    }
+
+    private static bool SameFrameworkSamePlatformLowerOrEqualPlatformVersion(NuGetFramework left, NuGetFramework right)
+    {
+        return SameFrameworkSamePlatform(left, right) &&
                left.PlatformVersion <= right.PlatformVersion;
     }
 }
