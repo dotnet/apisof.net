f263f7b47248b45741996c37a89b40876cc5b057
Immo Landwerth <immol@microsoft.com>
2024-05-06 21:57:58 -0700

    Simplify locating pack-based framework assemblies

--- src/Terrajobst.ApiCatalog.Generation/Frameworks/PackBasedFrameworkProvider.cs
+++ src/Terrajobst.ApiCatalog.Generation/Frameworks/PackBasedFrameworkProvider.cs
@@ -18,7 +18,7 @@ public sealed class PackBasedFrameworkProvider : FrameworkProvider
             yield break;
 
         var manifest = FrameworkManifest.Load(packIndexPath);
-        var assemblies = new Dictionary<string, FrameworkAssembly>(StringComparer.OrdinalIgnoreCase);
+        var assemblies = new List<FrameworkAssembly>();
 
         var entryByFx = manifest.Frameworks.ToDictionary(fx => NuGetFramework.Parse(fx.FrameworkName));
 
@@ -28,33 +28,16 @@ public sealed class PackBasedFrameworkProvider : FrameworkProvider
 
             AddAssemblies(entry.Packages, assemblies);
 
-            // TODO: I think this is superfluous now:
-            //
-            // For frameworks with a platform, such as `net5.0-windows`, we also want to add in all references
-            // from the base framework, such as `net5.0`.
-
-            var fx = NuGetFramework.Parse(entry.FrameworkName);
-            var baseFx = fx.GetBaseFramework();
-            if (baseFx is not null)
-            {
-                var baseEntry = entryByFx[baseFx];
-                AddAssemblies(baseEntry.Packages, assemblies);
-            }
-
-            var assemblyArray = assemblies.Values.ToArray();
-            yield return (entry.FrameworkName, assemblyArray);
+            yield return (entry.FrameworkName, assemblies.ToArray());
         }
 
-        static void AddAssemblies(IReadOnlyList<FrameworkManifestPackage> packages, Dictionary<string, FrameworkAssembly> assemblies)
+        static void AddAssemblies(IReadOnlyList<FrameworkManifestPackage> packages, List<FrameworkAssembly> assemblies)
         {
             foreach (var package in packages)
             foreach (var assembly in package.Assemblies)
             {
-                if (assemblies.ContainsKey(assembly.Path))
-                    continue;
-
                 var frameworkAssembly = new FrameworkAssembly(assembly.Path, package.Id, assembly.Profiles);
-                assemblies.Add(frameworkAssembly.Path, frameworkAssembly);
+                assemblies.Add(frameworkAssembly);
             }
         }
     }
