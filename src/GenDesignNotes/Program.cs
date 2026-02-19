fb0a320182c22ead8a94a4d23340554626b0f375
syrle-foronda <v-syforonda@microsoft.com>
2026-02-19 13:23:30 -0800

    publish wif artifact

--- src/GenDesignNotes/Program.cs
+++ src/GenDesignNotes/Program.cs
@@ -1,11 +1,9 @@
 ﻿using GenDesignNotes;
 using Microsoft.Extensions.Hosting;
 using Terrajobst.ApiCatalog.ActionsRunner;
-using Azure.Core.Diagnostics;
-using System.Diagnostics.Tracing;
+
 var builder = ConsoleHost.CreateApplicationBuilder();
 
-AzureEventSourceListener.CreateConsoleLogger(EventLevel.Verbose);
 builder.AddApisOfDotNetPathProvider();
 builder.AddApisOfDotNetStore();
 builder.AddApisOfDotNetWebHook();
