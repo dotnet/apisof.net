2c644802723503209e5d03dcdcd2e1777fd1175d
syrle-foronda <v-syforonda@microsoft.com>
2026-03-06 08:42:48 -0800

    remove extra opts

--- src/Terrajobst.ApiCatalog.ActionsRunner/ApisOfDotNetStoreOptions.cs
+++ src/Terrajobst.ApiCatalog.ActionsRunner/ApisOfDotNetStoreOptions.cs
@@ -6,6 +6,4 @@ public sealed class ApisOfDotNetStoreOptions
 {
     [Required]
     public required string AzureStorageServiceUrl { get; init; }
-    [Required]
-    public required string AzureClientId { get; init; }
 }
