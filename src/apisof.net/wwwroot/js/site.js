3a2a472db99aee08c0769a3ff8ec97ed01290e41
Kelvin-Macapagal <v-kmacapagal@microsoft.com>
2026-06-27 07:11:59 +0800

    remove hard coded display none

--- src/apisof.net/wwwroot/js/site.js
+++ src/apisof.net/wwwroot/js/site.js
@@ -3,6 +3,7 @@ var siteConsent = null;
 function setCookieManagementVisibility(isVisible) {
     var elements = document.querySelectorAll(".manageCookieChoice");
     for (var i = 0; i < elements.length; i++) {
+        elements[i].hidden = !isVisible;
         elements[i].style.display = isVisible ? "list-item" : "none";
     }
 }
