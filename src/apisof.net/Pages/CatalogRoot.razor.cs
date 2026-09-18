b59bc28d85dbb46fd694fbe79aa744f18734de23
Kelvin-Macapagal <v-kmacapagal@microsoft.com>
2026-09-18 20:00:53 +0800

    Refresh catalog root on query changes
    Subscribe CatalogRoot to query changes so the browsing context updates without reinitializing the page. Also refine diff filtering to respect IncludeChanged and keep APIs visible when nested changes are included.

--- src/apisof.net/Pages/CatalogRoot.razor.cs
+++ src/apisof.net/Pages/CatalogRoot.razor.cs
@@ -6,9 +6,13 @@ using Terrajobst.ApiCatalog;
 namespace ApisOfDotNet.Pages;
 
 public partial class CatalogRoot
+    : IDisposable
 {
     [Inject]
     public required CatalogService CatalogService { get; set; }
+
+    [Inject]
+    public required QueryManager QueryManager { get; set; }
 
     [Inject]
     public required NavigationManager NavigationManager { get; set; }
@@ -19,6 +23,12 @@ public partial class CatalogRoot
     public ApiBrowsingContext BrowsingContext { get; set; } = ApiBrowsingContext.Empty;
 
     protected override void OnInitialized()
+    {
+        QueryManager.QueryChanged += QueryManagerOnQueryChanged;
+        UpdateBrowsingContext();
+    }
+
+    private void UpdateBrowsingContext()
     {
         var query = BrowsingQuery.Get(CatalogService.Catalog, NavigationManager);
         var framework = query.Fx?.Framework;
@@ -34,5 +44,16 @@ public partial class CatalogRoot
             var diffOptions = query.DiffOptions?.DiffOptions;
             BrowsingContext = ApiBrowsingContext.ForFrameworkDiff(left, right, diffOptions, framework);
         }
+    }
+
+    private void QueryManagerOnQueryChanged(object? sender, IReadOnlySet<string> e)
+    {
+        UpdateBrowsingContext();
+        StateHasChanged();
+    }
+
+    public void Dispose()
+    {
+        QueryManager.QueryChanged -= QueryManagerOnQueryChanged;
     }
 }
